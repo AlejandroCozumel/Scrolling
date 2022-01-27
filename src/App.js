ea70d11283a8ec3e14313838ffd07fb78061fbe1
Alejandro Zamora <alejandroo14@gmail.com>
2022-01-27 01:10:18 -0500

    agregada imagen con animacion y terminada la segunda vista

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import "./css/style.css";
 import "./css/mac.css";
+import "./css/stripes.css";
 import Macbook from "./views/Macbook.jsx";
+import ImageStripe from "./views/ImageStripe.jsx";
 
 function App() {
-  return <div>
-    <Macbook />
-  </div>;
+  return (
+    <>
+      <Macbook />
+      <ImageStripe />
+    </>
+  );
 }
 
 export default App;
---
+++ src/views/ImageStripe.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Images() {
+  return <main>
+<bg-black>
+        <css-stripes>
+          <css-stripe cursor col1 data-title="css"></css-stripe>
+          <css-stripe cursor col2 data-title="is"></css-stripe>
+          <css-stripe cursor col3 data-title="art"></css-stripe>
+        </css-stripes>
+      </bg-black>
+  </main>;
+}
+
+export default Images;
